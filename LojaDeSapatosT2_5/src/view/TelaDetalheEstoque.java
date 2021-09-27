13eca15437e4bd523b64268418c713f98af89883
Gabriel Mariano <200018167@aluno.unb.br>
2021-09-27 17:52:02 -0300

    Update da Tela de Estoque
    Update da Tela de Estoque - criação de componentes

--- LojaDeSapatosT2_5/src/view/TelaDetalheEstoque.java
+++ LojaDeSapatosT2_5/src/view/TelaDetalheEstoque.java
@@ -29,6 +29,7 @@ public class TelaDetalheEstoque extends JFrame implements ActionListener {
 			"33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
 			"50" };
 	private String[] gen = { "M", "F", "Unissex" };
+	private String[] alturaSaltos = { "Baixo", "Médio", "Alto", "Não Possui" };
 
 	// Trabalhando com datas
 	private Date data = new Date();
@@ -42,40 +43,79 @@ public class TelaDetalheEstoque extends JFrame implements ActionListener {
 	private JLabel lblDataCadastro; // Data de Cadastro
 	private JLabel lblIdProd; // ID do Produto
 	private JLabel lblNome = new JLabel("Nome: "); // Nome
-	private JTextField valorNome = new JTextField();
+	private JTextField valorNome;
 	private JLabel lblTamanho = new JLabel("Tamanho: "); // Tamanho
 	private JComboBox valorTamanho = new JComboBox(tamanhos);
 	private JLabel lblPreco = new JLabel("Preço: "); // Preço
-	private JTextField valorPreco = new JTextField();
+	private JTextField valorPreco;
 	private JLabel lblMarca = new JLabel("Marca: "); // Marca
-	private JTextField valorMarca = new JTextField();
+	private JTextField valorMarca;
 	private JLabel lblOrigem = new JLabel("País de Origem: "); // País de Origem
-	private JTextField valorOrigem = new JTextField();
+	private JTextField valorOrigem;
 	private JLabel lblGarantia = new JLabel("Garantia: "); // Tempo de Garantia
-	private JTextField valorGarantia = new JTextField();
+	private JTextField valorGarantia;
 	private JLabel lblGenero = new JLabel("Gênero: "); // Gênero
 	private JComboBox valorGenero = new JComboBox(gen);
 	private JLabel lblBarras = new JLabel("Código de Barras: "); // Código de Barras
-	private JTextField valorBarras = new JTextField();
+	private JTextField valorBarras;
 	private JLabel lblCor = new JLabel("Cor: "); // Cor
-	private JTextField valorCor = new JTextField();
+	private JTextField valorCor;
 	private JLabel lblMaterial = new JLabel("Material: ");
-	private JTextField valorMaterial = new JTextField();
+	private JTextField valorMaterial;
 
 	// Declarando componentes específicos da tela
 	// SALTO
 	private JLabel lblSalto = new JLabel("Tipo de Salto: "); // Tipo de Salto
-	private JTextField valorSalto = new JTextField();
+	private JTextField valorSalto;
 	private JLabel lblCorExt = new JLabel("Cor Externa: "); // Cor externa
-	private JTextField valorCorExt = new JTextField();
+	private JTextField valorCorExt;
 	private JLabel lblCorSol = new JLabel("Cor do Solado: "); // Cor do Solado
-	private JTextField valorCorSol = new JTextField();
+	private JTextField valorCorSol;
 	private JLabel lblAltSalto = new JLabel("Altura do Salto: "); // Altura do Salto
-	private JTextField valorAltSalto = new JTextField();
-	// TENIS
+	private JTextField valorAltSalto;
 
-	public TelaDetalheEstoque(ControleDados d, int opcao, int idx) {
-		// TODO Auto-generated constructor stub
+	// TENIS
+	private JLabel lblAtividade = new JLabel("Atividade Indicada: "); // Atividade Indicada
+	private JTextField valorAtividade;
+	private JLabel lblTecSol = new JLabel("Tecnologia do Solado: "); // Tecnologia do Solado
+	private JTextField valorTecSol;
+	private JLabel lblPisada = new JLabel("Tipo de Pisada: "); // Tipo de Pisada
+	private JTextField valorPisada;
+	private JLabel lblTipoPalmilha = new JLabel("Tipo de Palmilha: "); // Tipo de Palmilha
+	private JTextField valorTipoPalmilha;
+	private JLabel lblPeso = new JLabel("Peso (g): "); // Peso
+	private JTextField valorPeso;
+	private JLabel lblAmarra = new JLabel("Tipo de Amarração: "); // Tipo de Amarração
+	private JTextField valorAmarra;
+	private JLabel lblCorCadarco = new JLabel("Cor do Cadarço: "); // Cor do Cadarço
+	private JTextField valorCorCadarco;
+
+	// BOTA
+	private JLabel lblModelo = new JLabel("Modelo: "); // Modelo
+	private JTextField valorModelo;
+	private JLabel lblAltCano = new JLabel("Altura do Cano: "); // Altura do Cano
+	private JTextField valorAltCano;
+	private JLabel lblTipoSalto = new JLabel("Tipo de Salto: "); // Tipo de Salto
+	private JTextField valorTipoSalto;
+	private JLabel lblTiPalmilha = new JLabel("Tipo de Palmilha: "); // Tipo de Palmilha
+	private JTextField valorTiPalmilha;
+	private JLabel lblMatSolado = new JLabel("Material do Solado: "); // Material do Solado
+	private JTextField valorMatSolado;
+
+	// SAPATO
+	private JLabel lblSolado = new JLabel("Material do Solado: "); // Material do Solado
+	private JTextField valorSolado;
+	private JLabel lblInterno = new JLabel("Material Interno: "); // Material Interno
+	private JTextField valorInterno;
+	private JLabel lblBico = new JLabel("Tipo de Bico: "); // Tipo de Bico
+	private JTextField valorBico;
+	private JLabel lblAlturaSalto = new JLabel("Altura do Salto: "); // Altura do Salto
+	private JComboBox valorAlturaSalto = new JComboBox(alturaSaltos);
+	private JLabel lblPalmilha = new JLabel("Tipo de Palmilha: "); // Tipo de Palmilha
+	private JTextField valorPalmilha;
+
+	public TelaDetalheEstoque(ControleDados d, int opcao, int idx, String tipoProd) {
+		super(tipoProd); // JFrame com o tipo do produto
 	}
 
 	@Override
