6ba43d9ea44498707ad8b8fea14c6d65b45cc6ff
B3holder2 <Pedro Torreo@DESKTOP-FFO0TOA>
2021-09-29 21:52:25 -0300

    testando token

--- LojaDeSapatosT2_5/src/view/TelaLoja.java
+++ LojaDeSapatosT2_5/src/view/TelaLoja.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
